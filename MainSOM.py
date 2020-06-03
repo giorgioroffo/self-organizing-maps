a1af122c901f0eb788f81e0cc0ed8fa72d938a42
roffogiorgio <roffo.giorgio@gmail.com>
2020-06-03 20:06:36 +0100

    template implementation unsupervised learning / using minisom library

---
+++ MainSOM.py
@@ -0,0 +1,49 @@
+# Self Organizing Map
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.preprocessing import MinMaxScaler
+
+from minisom import MiniSom
+
+from pylab import bone, pcolor, colorbar, plot, show
+
+# Importing the dataset
+dataset = pd.read_csv('./dataset/dataset_bank_credit_card_applications.csv')
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+# Training the SOM
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+# Visualizing the results
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+
+# Finding the frauds
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(8,1)], mappings[(6,8)]), axis = 0)
+frauds = sc.inverse_transform(frauds)
